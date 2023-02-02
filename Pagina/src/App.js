036c3c283c635975e743f4599672d2c3b3dfb374
Victor <victordevfront@gmail.com>
2023-02-01 21:31:16 -0300

    Criação da página

---
+++ Pagina/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Meu from './components/Meu/Meu';
+
+function App() {
+  return (
+    <Meu></Meu>
+
+  );
+}
+
+export default App;
---
+++ Pagina/src/components/Meu/Meu.js
@@ -0,0 +1,11 @@
+import "./Meu.css";
+
+function Meu() {
+  return (
+    <header className="meu">
+      <h1>Olá</h1>
+    </header>
+  )
+}
+//sempre exportar
+export default Meu
